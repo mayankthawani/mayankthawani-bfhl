db7ee8eca358f8d2fe4f9323efbf069985d9a030
Mayank Thawani <mayankthawani13@gmail.com>
2026-04-24 19:13:26 +0530

    third commit ading deployment url

--- bhfl-frontend/my-project/src/components/inputbox.jsx
+++ bhfl-frontend/my-project/src/components/inputbox.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/bfhl";
+const API_URL = "https://mayankthawani-bfhl.onrender.com/bfhl";
 
 export default function InputBox({ setResult }) {
   const [input, setInput] = useState("");
